8eaea8827368ce51b1bb97247c5b9662868ef66c
Aldo Puello Ramirez <112556371+aldopuello@users.noreply.github.com>
2023-04-18 15:18:53 -0500

    cambios

--- js/index.js
+++ js/index.js
@@ -1,34 +1,40 @@
-function getCaracteres(done){
 
-    const results = fetch("https://rickandmortyapi.com/api/character");
+const cardContainer = document.querySelector('.card-container');
 
-    results
-            .then(response => response.json())
-            .then(data => {
-                done(data)
-            });
-}
+// Hacemos una solicitud HTTP a la API de Rick and Morty
+fetch('https://rickandmortyapi.com/api/character/')
+  .then(response => response.json())
+  .then(data => {
+    // Tomamos los primeros 12 personajes
+    const characters = data.results.slice(0, 12);
 
-getCaracteres(data => {
-     
-    data.results.forEach(personaje => {
-        //  console.log(data); 
-        
-        const article = document.createRange().createContextualFragment(/*html*/`
-        
-        <article>
-            <div class="image-container">
-                <img src="${personaje.image}" alt="personaje">
-            </div>
-            <h2>${personaje.name}</h2>
-            <span>${personaje.status}</span>
-        </article>
+    // Generamos una tarjeta HTML para cada personaje
+    characters.forEach(character => {
+      const card = document.createElement('div');
+      card.classList.add('card');
 
-    `);
+      const image = document.createElement('img');
+      image.src = character.image;
+      image.alt = character.name;
+      card.appendChild(image);
 
-    const main = document.querySelector("main");
+      const name = document.createElement('h2');
+      name.textContent = character.name;
+      card.appendChild(name);
 
-    main.append(article);
+      const status = document.createElement('p');
+      status.textContent = `Estado: ${character.status}`;
+      card.appendChild(status);
+
+      const species = document.createElement('p');
+      species.textContent = `Especie: ${character.species}`;
+      card.appendChild(species);
+
+      const origin = document.createElement('p');
+      origin.textContent = `Origen: ${character.origin.name}`;
+      card.appendChild(origin);
+
+      cardContainer.appendChild(card);
     });
-    
-});
+  })
+  .catch(error => console.error(error));
